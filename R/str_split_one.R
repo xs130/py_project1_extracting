b0533591af655edfb004d1f2b7abe11e0e7bccb1
Xin Shen <xin@Xins-MBP.attlocal.net>
2022-08-21 12:01:48 -0700

    try again

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -13,6 +13,7 @@
 #'
 #' y <- "192.168.0.1"
 #' str_split_one(y, pattern = stringr::fixed("."))
+#' write this to understand push to git hub
 str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
